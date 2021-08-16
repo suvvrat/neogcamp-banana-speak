8e45af843d8e778525fddba20ab99198bba13e0b
Suvrat Verma <75042545+suvvrat@users.noreply.github.com>
2021-08-16 18:02:08 +0530

    javascript imported

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log("script is working from a different file");
+
+var username = prompt("give me your username");
+var welcomeMessage = "this script works ! " + username ;
+alert(welcomeMessage);
